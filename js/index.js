e8b4a28e8933dc28bf0538eb11664a3a874b1a7b
MDAM182 <mdam2345@gmail.com>
2019-05-01 18:23:00 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
+// created DOM element
 let element = document.createElement('div');
+//properties set
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
-document.body.appendChild(element);
 
+//appendChild()
+
+//append element to body
+document.body.appendChild(element);
+//center element
+element.style.textAlign = 'center';
+
+// append ul elements to DOM element
 let ul = document.createElement('ul');
 
 for (let i = 0; i < 3; i++) {
@@ -12,3 +21,15 @@ for (let i = 0; i < 3; i++) {
 }
 
 element.appendChild(ul);
+
+
+//moved ul elements to the left
+ul.style.textAlign = 'left';
+
+// removeChild()
+
+//remove second element from ul
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+//remove all elements from ul
+ul.remove();
